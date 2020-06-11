2cb2ad5c1b145b62b8667eccd54d30b730632404
Mahlori Ngobeni <mahlori@psybergate.co.za>
2020-06-11 08:01:51 +0000

    Merged in feature/RSM-312-as-a-web-client-i-need-my-gatewa (pull request #1)
    progress on inital gateway api
    
    Approved-by: Thabani <thabani@psybergate.co.za>

--- src/main/java/com/psybergate/gateway/GatewayApplication.java
+++ src/main/java/com/psybergate/gateway/GatewayApplication.java
@@ -2,7 +2,12 @@ package com.psybergate.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
